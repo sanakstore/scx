0cf5602b455f75071467e94ab3630df99bd697eb
sanakstore <144905341+sanakstore@users.noreply.github.com>
2024-02-25 21:22:05 +0800

    sanak store

--- deb10.sh
+++ deb10.sh
@@ -913,6 +913,11 @@ cat >/etc/cron.d/xp_all <<-END
 		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 		0 0 * * * root /usr/local/sbin/xp
 	END
+cat >/etc/cron.d/autobckp <<-END
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		0 0 * * * root /usr/local/sbin/autobckp
+	END
 	cat >/etc/cron.d/logclean <<-END
 		SHELL=/bin/sh
 		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
